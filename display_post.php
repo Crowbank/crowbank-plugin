adea3a3c8a2fbbb39f1a44ec6c2cc2f0dbac7391
the_y <the_y@HP-SERVER>
2018-08-18 21:25:45 +0100

    Added W3.CSS to display_post

--- display_post.php
+++ display_post.php
@@ -1,23 +1,19 @@
 <?php
 echo '
 <html>
+<head>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
 <body>
+
+<div class="w3-container">';
+
+echo htmlentities('crowbank.partners@gmail.com');
+echo '</div>
+
+<div class="w3-container">
 <h1>SERVER</h1>
-<table>
-<thead>
-<th>Key</th>
-<th>Value</th>
-</thead>
-<tbody>';
-foreach ($_SERVER as $key => $value) {
-	echo '<tr><td>' . $key . '</td><td>' . $value . '</td></tr>';
-}
-
-echo '</tbody>
-</table>
-
-<h1>REQUEST</h1>
-<table>
+<table class="w3-table w3-striped w3-border">
 <thead>
 <th>Key</th>
 <th>Value</th>
@@ -29,7 +25,23 @@ foreach ($_REQUEST as $key => $value) {
 
 echo '</tbody>
 </table>
+</div>
 
+<div class="w3-container">
+<h1>REQUEST</h1>
+<table class="w3-table w3-striped w3-border">
+<thead>
+<th>Key</th>
+<th>Value</th>
+</thead>
+<tbody>';
+foreach ($_SERVER as $key => $value) {
+	echo '<tr><td>' . $key . '</td><td>' . $value . '</td></tr>';
+}
+
+echo '</tbody>
+</table>
+</div>
 
 </body>
 </html>';
